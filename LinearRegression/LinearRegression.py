37ffabb7a51bc33f6a476bdb4e3150ce846c0f45
raselpy <rasel.py@gmail.com>
2024-11-18 01:07:49 +0100

    feat: add parameter initialization for LinearRegression
    - Implemented `initialize_parameters` method to initialize model parameters:
      - `weights` initialized as a zero array based on the number of features.
      - `bias` initialized to 0.

--- LinearRegression/LinearRegression.py
+++ LinearRegression/LinearRegression.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 class LinearRegression(object):
 
     def __init__(self, learning_rate=0.01, n_iterations=1000):
@@ -5,6 +8,10 @@ class LinearRegression(object):
         self.n_iterations = n_iterations
         self.weights = None
         self.bias = None
+
+    def initialize_parameters(self, n_features):
+        self.weights = np.zeros(n_features)
+        self.bias = 0
 
     def fit(self, X, y):
         pass
