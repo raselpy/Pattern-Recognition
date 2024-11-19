cf5a2bb7d47d26978c0870a55a9c2d08db93ae20
raselpy <rasel.py@gmail.com>
2024-11-20 00:41:08 +0100

    feat: add __init__ method for class initialization
    - Added parameters `learning_rate` and `n_iter` with default values of 0.01 and 1000, respectively.
    - Initialized `weights` and `bias` attributes to None.
    - Prepared the class for customizable training with specified learning rate and iterations.

--- logisticRegression/logisticRegression.py
+++ logisticRegression/logisticRegression.py
@@ -1,6 +1,12 @@
+from sklearn.decomposition import non_negative_factorization
+
+
 class LogisticRegression(object):
-    def __init__(self):
-        pass
+    def __init__(self,learning_rate=0.01,n_iter=1000):
+        self.learning_rate = learning_rate
+        self.n_iter = n_iter
+        self.weights = None
+        self.bias = None
 
     def sigmoid(self):
         pass
