b7ccd68a534667105f1ec661d9e9e536b81e47a6
raselpy <rasel.py@gmail.com>
2024-11-24 18:06:51 +0100

    feat(model): initialization initialize the number of neighbors  and Implemented the `fit` method to store training data (`X` and `y`)

--- k-Nearest Neighbors/K-Nearest Neighbors.py
+++ k-Nearest Neighbors/K-Nearest Neighbors.py
@@ -1,9 +1,11 @@
-class KNN:
-    def __init__(self):
-        pass
+class KNN(object):
+    def __init__(self, n_neighbor=3):
+        self.n_neighbor = n_neighbor
 
-    def fit(self):
-        pass
+    def fit(self, X, y):
+        self.X = X
+        self.y = y
+        return self
 
     def predict(self):
         pass
