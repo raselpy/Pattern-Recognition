1cb4bb3157d72c1dded2587af28234e86b1fbd86
raselpy <rasel.py@gmail.com>
2024-12-02 11:43:48 +0100

    feat(model): Add initializer for SVM class

--- svm/svm_classifier.py
+++ svm/svm_classifier.py
@@ -1,6 +1,10 @@
 class SVM(object):
-    def __init__(self):
-        pass
+    def __init__(self,learning_rate=0.01, lambda_param=0.01, n_iters=1000):
+        self.lr = learning_rate
+        self.lambda_param = lambda_param
+        self.n_iters = n_iters
+        self.w = None
+        self.b = None
 
     def fit(self):
         pass
